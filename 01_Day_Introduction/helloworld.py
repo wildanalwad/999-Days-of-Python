981cb3051c8464bfa74df1ce5284b5cf69a17b74
Wildan Ghaly Alwadhify <86901137+wildanalwad@users.noreply.github.com>
2023-09-13 10:40:45 +0700

    Add files via upload

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,20 @@
+# Introduction
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('WildanAlwad'))          # String
+print(type([1, 2, 3]))           # List
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type({'name':'WildanAlwad'})) # Dictionary
+print(type((9.8, 3.14, 2.7)))    # Tuple
